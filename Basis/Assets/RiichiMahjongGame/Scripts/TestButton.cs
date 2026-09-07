9f72d308fdbb92681869be1c4c92924bd7dd188f
hetarikia <155637463+hetarikia@users.noreply.github.com>
2026-09-07 15:07:27 -0400

    Convert to cilbox, add hetarikia namespace

--- Basis/Assets/RiichiMahjongGame/Scripts/TestButton.cs
+++ Basis/Assets/RiichiMahjongGame/Scripts/TestButton.cs
@@ -1,20 +1,33 @@
 using System;
+using Basis.Scripts.BasisSdk.Interactions;
+using Basis.Scripts.Device_Management.Devices;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 
-public class TestButton : MonoBehaviour
+namespace hetarikia.Mahjong
 {
-	[SerializeField]
-	private Transform tiles;
-
-	public void OnButtonClick()
+	[Cilboxable]
+	public class TestButton : MonoBehaviour
 	{
-		for (int i = tiles.childCount - 1; i >= 1; i--)
+		[SerializeField]
+		private Transform tiles;
+		private BasisInteractableButton shuffleButton;
+
+		void Start()
 		{
-			Transform randomTile = tiles.GetChild(UnityEngine.Random.Range(0, i));
-			Vector3 tempTilePosition = tiles.GetChild(i).position;
-			tiles.GetChild(i).position = randomTile.position;
-			randomTile.position = tempTilePosition;
+			shuffleButton = GetComponent<BasisInteractableButton>();
+			shuffleButton.OnInteractStartEvent.AddListener(OnButtonClick);
+		}
+
+		private void OnButtonClick(BasisInput input)
+		{
+			for (int i = tiles.childCount - 1; i >= 1; i--)
+			{
+				Transform randomTile = tiles.GetChild(UnityEngine.Random.Range(0, i));
+				Vector3 tempTilePosition = tiles.GetChild(i).position;
+				tiles.GetChild(i).position = randomTile.position;
+				randomTile.position = tempTilePosition;
+			}
 		}
 	}
 }
---
+++ Basis/Assets/RiichiMahjongGame/Scripts/Tile.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace hetarikia.Mahjong
+{
+	[Cilboxable]
+	public class Tile : MonoBehaviour
+	{
+		public int value;
+	}
+}
