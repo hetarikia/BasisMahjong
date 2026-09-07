9e95ba2f30f0a8f8276dcc3ab72c558119bca5b4
hetarikia <155637463+hetarikia@users.noreply.github.com>
2026-09-07 13:47:24 -0400

    Local shuffle implemented

--- Basis/Assets/RiichiMahjongGame/Scripts/TestButton.cs
+++ Basis/Assets/RiichiMahjongGame/Scripts/TestButton.cs
@@ -1,10 +1,20 @@
+using System;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class TestButton : MonoBehaviour
 {
+	[SerializeField]
+	private Transform tiles;
 
-    public void OnButtonClick()
-    {
-        Debug.Log("clicked!");
-    }
+	public void OnButtonClick()
+	{
+		for (int i = tiles.childCount - 1; i >= 1; i--)
+		{
+			Transform randomTile = tiles.GetChild(UnityEngine.Random.Range(0, i));
+			Vector3 tempTilePosition = tiles.GetChild(i).position;
+			tiles.GetChild(i).position = randomTile.position;
+			randomTile.position = tempTilePosition;
+		}
+	}
 }
